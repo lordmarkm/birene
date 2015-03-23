f023bd9ddf5afbea50df4e8ea8111931adb27640
mbmartinez <mbmartinez@exist.com>
2015-03-23 13:02:35 +0800

    fasdf

--- birene-web/src/main/webapp/modules/course/controller/CourseSearchController.js
+++ birene-web/src/main/webapp/modules/course/controller/CourseSearchController.js
@@ -1,6 +1,25 @@
 define(function () {
   return ['$scope', 'ngTableParams', 'CourseService',
     function ($scope, ngTableParams, CourseService) {
-    }
+
+    //Taxis table
+    $scope.tableParams = new ngTableParams({
+      page: 0,
+      count: 5,
+      sorting: {
+        trackingNo: 'asc'
+      }
+    }, {
+      total: 0,
+      counts: [5,10,15],
+      getData: function($defer, params) {
+        CourseService.get(params.$params, function(response) {
+          params.total(response.total);
+          $defer.resolve(response.data);
+        });
+      }
+    });
+
+  }
   ];
 });
